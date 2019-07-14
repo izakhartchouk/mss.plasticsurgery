e3a07696eda95f785329058dac4d31e0c4ef0ac9
Ivan Zakhartchouk <ivannn787@gmail.com>
2019-07-15 01:27:43 +0300

    MPS-0001: Added basic Admin page
    - added Admin controller

---
+++ MSS.PlasticSurgery/Controllers/AdministrationController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MSS.PlasticSurgery.Models;
+
+namespace MSS.PlasticSurgery.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
